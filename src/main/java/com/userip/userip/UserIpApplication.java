75181710ee906143bfd920098fc71cca0d778480
김윤민 <dbsalszz@naver.com>
2022-12-11 18:05:09 +0900

    Spring_UserIP조회하는 api

---
+++ src/main/java/com/userip/userip/UserIpApplication.java
@@ -0,0 +1,13 @@
+package com.userip.userip;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserIpApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UserIpApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/userip/userip/UserIpApplicationTests.java
@@ -0,0 +1,13 @@
+package com.userip.userip;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserIpApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
